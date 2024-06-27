11fb8de7cf4c1ec9a236d728743c34b03e91a988
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2024-06-27 13:48:16 -0400

    Update phylib.c

--- phylib.c
+++ phylib.c
@@ -642,3 +642,49 @@ phylib_table *phylib_segment(phylib_table *table)
     //Return the table
     return segTable;
 }
+
+char *phylib_object_string( phylib_object *object ) {
+    static char string[80];
+    if (object==NULL) {
+        snprintf( string, 80, "NULL;" );
+        return string;
+    }
+
+    switch (object->type) {
+        case PHYLIB_STILL_BALL:
+            snprintf( string, 80,
+            "STILL_BALL (%d,%6.1lf,%6.1lf)",
+            object->obj.still_ball.number,
+            object->obj.still_ball.pos.x,
+            object->obj.still_ball.pos.y );
+        break;
+        case PHYLIB_ROLLING_BALL:
+            snprintf( string, 80,
+            "ROLLING_BALL (%d,%6.1lf,%6.1lf,%6.1lf,%6.1lf,%6.1lf,%6.1lf)",
+            object->obj.rolling_ball.number,
+            object->obj.rolling_ball.pos.x,
+            object->obj.rolling_ball.pos.y,
+            object->obj.rolling_ball.vel.x,
+            object->obj.rolling_ball.vel.y,
+            object->obj.rolling_ball.acc.x,
+            object->obj.rolling_ball.acc.y );
+        break;
+        case PHYLIB_HOLE:
+            snprintf( string, 80,
+            "HOLE (%6.1lf,%6.1lf)",
+            object->obj.hole.pos.x,
+            object->obj.hole.pos.y );
+        break;
+        case PHYLIB_HCUSHION:
+            snprintf( string, 80,
+            "HCUSHION (%6.1lf)",
+            object->obj.hcushion.y );
+        break;
+        case PHYLIB_VCUSHION:
+            snprintf( string, 80,
+            "VCUSHION (%6.1lf)",
+            object->obj.vcushion.x );
+        break;
+    }
+    return string;
+}
